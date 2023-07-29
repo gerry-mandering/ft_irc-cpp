4009438e773acbf18fc79f8ceb2f2e7c1c4f97d3
gerry-mandering <kimmen1119@gmail.com>
2023-07-29 22:38:50 +0900

    Feat: remove comment on Request constructor

--- srcs/request/Request.cpp
+++ srcs/request/Request.cpp
@@ -2,13 +2,11 @@
 
 Request::Request(handle_t socket) : mSocket(socket)
 {
-    // ClientRepository *clientRepository = ClientRepository::GetInstance();
-    // mClient = clientRepository->FindBySocket(socket);
+    ClientRepository *clientRepository = ClientRepository::GetInstance();
+    mClient = clientRepository->FindBySocket(socket);
 
-    // if (!mClient)
-    // mClient = clientRepository->CreateClient(socket);
-    // TODO: 하단 줄 삭제
-    (void)mSocket;
+    if (!mClient)
+        mClient = clientRepository->CreateClient(socket);
 }
 
 Client *Request::GetClient() const
