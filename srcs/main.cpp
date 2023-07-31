e545af24d0ac89870046b199af335e095c49ede3
geneeol <trumpjqk@daum.net>
2023-07-31 22:40:55 +0900

    Feat: main문 input 유효성 검사 추가

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -7,16 +7,40 @@
 #include <iostream>
 #include <string>
 
-#define PORT 6667
-#define PASSWORD "1234"
+#define PORT_LEN 5
+#define PASSWORD_LEN 15
+
+static bool invalid_input(const std::string &port, const std::string &password)
+{
+    if (port.length() > PORT_LEN || password.length() > PASSWORD_LEN)
+        return (true);
+    for (size_t i = 0; i < port.length(); i++)
+    {
+        if (!std::isdigit(port[i]))
+            return (true);
+    }
+    for (size_t i = 0; i < password.length(); i++)
+    {
+        if (!std::isalnum(password[i]))
+            return (true);
+    }
+    return (false);
+}
 
 int main(int argc, char **argv)
 {
-    (void)argv;
+    // TODO: singleton atexit 처리 고민할것
     if (argc != 3)
     {
-        std::cerr << "Wrong Parameter." << std::endl;
-        return (0);
+        std::cerr << "Usage: <port> <password>\n";
+        return (EXIT_FAILURE);
+    }
+    if (invalid_input(argv[1], argv[2]))
+    {
+        std::cerr << "Input is invalid\n"
+                  << "port should be integer and password should be alphanumeric\n";
+        return (EXIT_FAILURE);
+    }
     }
     EnvManager::GetInstance()->SetPortNumber(std::string(argv[1]));
     EnvManager::GetInstance()->SetConnectionPassword(std::string(argv[2]));
